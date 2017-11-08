78268de7bdcd0bceebce1fb9a139df14f138cd63
Robert Cini <b31271@mcast.org.mt>
2017-11-08 12:15:08 +0100

    -Added Colliders to Ball and Paddle -Added Lose Collider which will only be a trigger -Upon triggering lose collider, we loaded the lose scene.

---
+++ Brick Breaker 4.2D/Brick Breaker 4.2D/Assets/Scripts/LoseColliderScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseColliderScript : MonoBehaviour {
+
+    LevelManager levelmanager = new LevelManager();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Trigger");
+    }
+}
--- Brick Breaker 4.2D/Brick Breaker 4.2D/Assets/Scripts/MusicPlayerScript.cs
+++ Brick Breaker 4.2D/Brick Breaker 4.2D/Assets/Scripts/MusicPlayerScript.cs
@@ -3,25 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicPlayerScript : MonoBehaviour {
+
     static MusicPlayerScript instance = null;
-    // Use this for initialization
 
     private void Awake()
     {
         if (instance == null)
         {
-            instance = this; //this current object
+            instance = this; // this current object (my Music Player)
             DontDestroyOnLoad(gameObject);
         }
-        else // if not null then a music player is already running
+        else // if not null, then a Music Player is already running
         {
             Destroy(gameObject);
-            print("Duplicate Music Player is self destructing");
+            print("Duplicate Music Player is self destructing!");
         }
     }
+
+    // Use this for initialization
     void Start () {
-        
-        
+
 	}
 	
 	// Update is called once per frame
