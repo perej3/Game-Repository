e50f44501e1c432348d7f66b277c5948b6a10e04
Robert Cini <b31271@mcast.org.mt>
2017-11-08 10:44:13 +0100

    Added Brick Breaker

---
+++ Brick Breaker 4.2D/Brick Breaker 4.2D/Assets/Scripts/MusicPlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayerScript : MonoBehaviour {
+    static MusicPlayerScript instance = null;
+    // Use this for initialization
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this; //this current object
+            DontDestroyOnLoad(gameObject);
+        }
+        else // if not null then a music player is already running
+        {
+            Destroy(gameObject);
+            print("Duplicate Music Player is self destructing");
+        }
+    }
+    void Start () {
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
