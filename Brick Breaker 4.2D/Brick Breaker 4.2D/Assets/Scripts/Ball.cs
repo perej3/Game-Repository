f61bfe3534698d1a75b35b2e4f1ab0460923962f
Robert Cini <b31271@mcast.org.mt>
2017-11-10 14:24:58 +0100

    - Set paddle to move with mouse position (in game units) - Lock the ball to move with the paddle at the start of the game - Upon first left mouse button click, the ball should launch.

---
+++ Brick Breaker 4.2D/Brick Breaker 4.2D/Assets/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public Paddle paddle;
+    Vector3 paddleBallPosDiff;
+    bool gameStarted = false;
+	// Use this for initialization
+	void Start () {
+        paddleBallPosDiff = this.transform.position - paddle.transform.position; 		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!gameStarted)
+        {
+            this.transform.position = paddle.transform.position + paddleBallPosDiff;
+        }
+        if (Input.GetMouseButtonDown(0) && !gameStarted)
+        {
+            gameStarted = true;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+        }
+	}
+}
