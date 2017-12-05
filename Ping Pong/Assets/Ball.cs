1ce28a3565b76d7fb0572be95329e516a6df628b
perej3 <robert.cini.b31271@mcast.edu.mt>
2017-12-05 17:09:49 +0100

    -- Added a script for the ball and behaviour for bouncing -- Added a script for the paddle to move with the mouse -- Fixed an issue where the mouse was not in sync with paddle -- Fixed an issue where the ball was sticking to the paddle

---
+++ Ping Pong/Assets/Ball.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
---
+++ Ping Pong/Assets/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mousePosInUnits = (Input.mousePosition.y / Screen.width * 16) -6;
+
+        Vector3 newPaddlePos = new Vector3(this.transform.position.x, mousePosInUnits, this.transform.position.z);
+        newPaddlePos.y = Mathf.Clamp(mousePosInUnits, -4.36f, 4.36f);
+
+        this.transform.position = newPaddlePos;
+    }
+}
