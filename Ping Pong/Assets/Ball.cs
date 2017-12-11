6e5eef58c5c926d3441f96126167722da0c69bcd
perej3 <robert.cini.b31271@mcast.edu.mt>
2017-12-11 19:48:23 +0100

    -- Added Script for second player -- Added Clamp for second player's paddle. -- Added Randomised directions for the ball to start. -- Added Score to the game.

--- Ping Pong/Assets/Ball.cs
+++ Ping Pong/Assets/Ball.cs
@@ -1,16 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class Ball : MonoBehaviour {
+public class Ball : MonoBehaviour
+{
+    Vector3 originalPos;
+   
 
-	// Use this for initialization
-	void Start () {
-        this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+    // Use this for initialization
+    void Start()
+    { 
+        originalPos = gameObject.transform.position;
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2((Random.Range(-11, 11)), 7f);
+
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    
+    int score1;
+    int score2;
+    
+    
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "goal_right")
+        {
+            score1++;
+            
+            gameObject.transform.position = originalPos;
+
+            
+        }
+       else if (coll.gameObject.name == "goal_left")
+        {
+            score2++;
+            
+            gameObject.transform.position = originalPos;
+            
+        }
+
+    }
+      
+    // Update is called once per frame
+    void Update()
+    {
         
+
+
     }
+   
 }
---
+++ Ping Pong/Assets/Paddle2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle2 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            
+            Vector3 position = this.transform.position;
+           position.y =  position.y + 0.5f;
+            position.y = Mathf.Clamp(position.y, -4.36f, 4.36f);
+            
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            
+            Vector3 position = this.transform.position;
+            position.y = position.y - 0.5f;
+            position.y = Mathf.Clamp(position.y, -4.36f, 4.36f);
+            
+            this.transform.position = position;
+            
+        }
+    }
+}
